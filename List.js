6f463ddf0916b2bff2e5b01e0724c64c72f7ae45
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-07-07 13:34:14 -0500

    added eslint and updated style to conform

--- List.js
+++ List.js
@@ -1,26 +1,29 @@
 
 export default class List {
-	constructor(items){
-		this.items = items;
-	}
+  constructor(items) {
+    this.items = items;
+  }
 
-	min(){
-		return Math.min.apply(null, this.items);
-	}
+  min() {
+    return Math.min.apply(null, this.items);
+  }
 
-	max(){
-		return Math.max.apply(null, this.items);
-	}
+  max() {
+    return Math.max.apply(null, this.items);
+  }
 
-	average(){
-		return this.items.length ? this.sum() / this.items.length : 0;
-	}
+  average() {
+    return this.items.length ? this.sum() / this.items.length : 0;
+  }
 
-	sum(){
-		var result = 0, l = this.length;
-		if (l) while (l--){
-			if (this[l] != null) result += parseFloat(this[l]);
-		}
-		return result;
-	}
+  sum() {
+    let result = 0;
+    let l = this.length;
+    if (l) {
+      while (l--) {
+        if (this[l] != null) result += parseFloat(this[l]);
+      }
+    }
+    return result;
+  }
 }
--- __tests__/List-test.js
+++ __tests__/List-test.js
@@ -4,12 +4,8 @@ jest.unmock('../List');
 import List from '../List';
 
 describe('List', () => {
-
-	it('.... change ....', () => {
-
-		// remove and add your expectations
-		expect(false).toBe(true);
-
-	});
-
+  it('.... change ....', () => {
+    // remove and add your expectations
+    expect(false).toBe(true);
+  });
 });
