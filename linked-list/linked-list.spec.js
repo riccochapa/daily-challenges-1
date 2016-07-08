e7b70943fa4a96a4d35f1c207c7c96430559729f
Olmo Maldonado <olmo.maldonado@gmail.com>
2016-07-08 11:15:43 -0500

    add linked-list

---
+++ linked-list/linked-list.spec.js
@@ -0,0 +1,70 @@
+import { LinkedList } from './LinkedList';
+
+describe('LinkedList', () => {
+  it('push/pop', () => {
+    const list = new LinkedList();
+    list.push(10);
+    list.push(20);
+    expect(list.pop()).toBe(20);
+    expect(list.pop()).toBe(10);
+  });
+  it('push/shift', () => {
+    const list = new LinkedList();
+    list.push(10);
+    list.push(20);
+    expect(list.shift()).toBe(10);
+    expect(list.shift()).toBe(20);
+  });
+  it('unshift/shift', () => {
+    const list = new LinkedList();
+    list.unshift(10);
+    list.unshift(20);
+    expect(list.shift()).toBe(20);
+    expect(list.shift()).toBe(10);
+  });
+  it('unshift/pop', () => {
+    const list = new LinkedList();
+    list.unshift(10);
+    list.unshift(20);
+    expect(list.pop()).toBe(10);
+    expect(list.pop()).toBe(20);
+  });
+  it('example', () => {
+    const list = new LinkedList();
+    list.push(10);
+    list.push(20);
+    expect(list.pop()).toBe(20);
+    list.push(30);
+    expect(list.shift()).toBe(10);
+    list.unshift(40);
+    list.push(50);
+    expect(list.shift()).toBe(40);
+    expect(list.pop()).toBe(50);
+    expect(list.shift()).toBe(30);
+  });
+  it('can count its elements', () => {
+    const list = new LinkedList();
+    expect(list.count()).toBe(0);
+    list.push(10);
+    expect(list.count()).toBe(1);
+    list.push(20);
+    expect(list.count()).toBe(2);
+  });
+  it('deletes the last element from the list', () => {
+    const list = new LinkedList();
+    list.push(10);
+    list.push(20);
+    list.push(30);
+    list.delete(20);
+    expect(list.count()).toBe(2);
+    expect(list.pop()).toBe(30);
+    expect(list.shift()).toBe(10);
+  });
+  it('deletes the only element', () => {
+    const list = new LinkedList();
+    list.push(10);
+    list.delete(10);
+
+    expect(list.count()).toBe(0);
+  });
+});
