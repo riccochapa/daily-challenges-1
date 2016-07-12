336853534cd1cb7b3dfbb2aab73c0b5ec1492830
Ricco <riccochapa@gmail.com>
2016-07-12 11:05:57 -0500

    completed assignment
    passed all jasmine tests.

--- atbash-cipher/atbash-cipher.js
+++ atbash-cipher/atbash-cipher.js
@@ -1,5 +1,20 @@
 function encode(string) {
-  return '';
+  const alpha = 'abcdefghijklmnopqrstuvwxyz';
+  const cipher = 'zyxwvutsrqponmlkjihgfedcba';
+
+  const alphaSplit = alpha.split('');
+  const cipherSplit = cipher.split('');
+  const stringSplit = string.toLowerCase().split('');
+  const conversion = [];
+
+  for (let i = 0; i < stringSplit.length; i++) {
+    for (let j = 0; j < alphaSplit.length; j++) {
+      const letter = stringSplit[i];
+      if (letter === alphaSplit[j]) {
+        conversion.push(cipherSplit[j]);
+      }
+    }
+  }
 }
 
 module.exports = { encode };
