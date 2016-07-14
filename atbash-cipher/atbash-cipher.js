3a33b89176fcf8979ec20e2a62083a497d2cd292
Ricco <riccochapa@gmail.com>
2016-07-13 19:45:38 -0500

    complete.

--- atbash-cipher/atbash-cipher.js
+++ atbash-cipher/atbash-cipher.js
@@ -1,24 +1,30 @@
 function encode(string) {
-  const alpha = 'abcdefghijklmnopqrstuvwxyz';
-  const cipher = 'zyxwvutsrqponmlkjihgfedcba';
+  const alpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const cipher = 'zyxwvutsrqponmlkjihgfedcba0123456789';
 
   const alphaSplit = alpha.split('');
   const cipherSplit = cipher.split('');
   const stringSplit = string.toLowerCase().split('');
   const conversion = [];
+  let counter = 0;
 
+  let result = '';
   for (let i = 0; i < stringSplit.length; i++) {
     for (let j = 0; j < alphaSplit.length; j++) {
-      const letter = stringSplit[i];
-      if (letter === alphaSplit[j]) {
+      if (stringSplit[i] === alphaSplit[j]) {
+        if (counter >= 5) {
+          counter = 0;
+          conversion.push(' ');
+        }
         conversion.push(cipherSplit[j]);
+        counter++;
       }
-      const result = conversion;
-      console.log(result);
+      result = conversion.join('');
     }
   }
+  return result;
 }
 
-encode('hello');
-encode('svool');
+encode('no');
+encode('yes');
 module.exports = { encode };
